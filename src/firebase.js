c030a085a7c091c896ada9d0d4b802d0aa27749c
RafayShams <rafayshams@gmail.com>
2023-06-12 19:47:25 -0400

    Added all components. Completed Header, Footer and started Start component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAW6N06y56bVltYy4EVo9Qxg63uRRC-38Q",
+  authDomain: "box-office-boffo.firebaseapp.com",
+  projectId: "box-office-boffo",
+  storageBucket: "box-office-boffo.appspot.com",
+  messagingSenderId: "223141360184",
+  appId: "1:223141360184:web:35b32ae93ab82eb2d56ccd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
