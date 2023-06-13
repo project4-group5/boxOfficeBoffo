4dad32a89b1f016d820edebe34d97404a14568ca
anya-uwu <anyadev@proton.me>
2023-06-12 21:24:32 -0400

    added more JSX to components

---
+++ src/components/MovieCard.js
@@ -0,0 +1,15 @@
+const MovieCard = (props) => {
+    return (
+        <li>
+            <div className="imageContainer">
+                <img src="" alt="" />
+            </div>
+            <div className="buttonContainer">
+                <button>ⓘ</button>
+                <button>+</button>
+            </div>
+        </li>
+    )
+}
+
+export default MovieCard;
