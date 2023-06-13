4174d5ffd91c2bfccc72ff664ac13558c7209aa8
MxCabrera <migueledward@hotmail.com>
2023-06-12 21:03:46 -0400

    added routes for Ranking, added content to rankings.js & gallery.js

---
+++ src/components/Ranking.js
@@ -0,0 +1,35 @@
+const Ranking = () => {
+   const listSlots = {
+      one: "",
+      two: "",
+      three: "",
+      four: "",
+      five: "",
+      six: "",
+      seven: "",
+      eight: "",
+      nine: "",
+      ten: ""
+   }
+   
+   return (
+      <section>
+         {/* <ul>
+
+            <li>{listSlots.one}Hey</li>
+            <li>{listSlots.two}</li>
+            <li>{listSlots.three}</li>
+            <li>{listSlots.four}</li>
+            <li>{listSlots.five}</li>
+            <li>{listSlots.six}</li>
+            <li>{listSlots.seven}</li>
+            <li>{listSlots.eight}</li>
+            <li>{listSlots.nine}</li>
+            <li>{listSlots.ten}</li>
+         </ul> */}
+         <p>HEY!</p>
+      </section>
+   )
+}
+
+export default Ranking;
