c28511c5442ae9f58e44f8508f78ea03becdf1d3
SilviaZlo <silvianevesmayer@gmail.com>
2023-06-22 20:54:30 -0400

    Added useState to App.js component and passed it as props to Raning.js component.We also added up and down buttons to Ranking.js component.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import Gallery from './components/Gallery';
 import { Route, Routes } from 'react-router-dom';
 import Ranking from './components/Ranking';
 import MovieDetails from './components/MovieDetails';
+import { useState } from 'react';
 
 function App() {
+
+  const [ userList, setUserList ] = useState(
+      {
+        one: "Click to add movie",
+        two: "Click to add movie",
+        three: "Click to add movie",
+        four: "Click to add movie",
+        five: "Click to add movie",
+        six: "Click to add movie",
+        seven: "Click to add movie",
+        eight: "Click to add movie",
+        nine: "Click to add movie",
+        ten: "Click to add movie"
+      }
+  )
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Start />}/>
-        <Route path="/Rankings/:userYear" element={<Ranking />}/>
+        <Route path="/Rankings/:userYear" element={<Ranking userList={userList} />}/>
         <Route path="/Gallery/:userYear" element={<Gallery />}/>
         <Route path="/MovieDetails/:movieId" element={<MovieDetails />} />
       </Routes>
--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,46 +1,36 @@
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import OrderButtons from "./OrderButtons";
 
-const Ranking = () => {
+const Ranking = (props) => {
    const navigate = useNavigate();
    const { userYear } = useParams();
 
    const handleClick = () => {
       navigate(`/Gallery/${userYear}`);
    }
-   
 
-   const listSlots = {
-      one: "",
-      two: "",
-      three: "",
-      four: "",
-      five: "",
-      six: "",
-      seven: "",
-      eight: "",
-      nine: "",
-      ten: ""
-   }
-   
+
    return (
       <section>
+         <div>
+            <h2>Highest grossing movies of year {userYear}:</h2>
+         </div>
          <div className="editButton">
             <button>Edit</button>
          </div>
          <ol>
-            <li onClick={handleClick}>{listSlots.one}Hey</li>
-            <li onClick={handleClick}>{listSlots.two}</li>
-            <li onClick={handleClick}>{listSlots.three}</li>
-            <li onClick={handleClick}>{listSlots.four}</li>
-            <li onClick={handleClick}>{listSlots.five}</li>
-            <li onClick={handleClick}>{listSlots.six}</li>
-            <li onClick={handleClick}>{listSlots.seven}</li>
-            <li onClick={handleClick}>{listSlots.eight}</li>
-            <li onClick={handleClick}>{listSlots.nine}</li>
-            <li onClick={handleClick}>{listSlots.ten}</li>
+            <li><p onClick={handleClick}>{props.userList.one}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.three}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.two}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.four}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.five}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.six}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.seven}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.eight}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.nine}</p><OrderButtons /></li>
+            <li><p onClick={handleClick}>{props.userList.ten}</p><OrderButtons /></li>
          </ol>
-         <p>HEY!</p>
          <div className="buttonContainer">
             <button>Clear List</button>
             <button>Lock In</button>
